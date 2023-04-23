1b107bead8af214e7186c3ee906ec3847c392682
Neillcllghn <neillcllghn@outlook.com>
2023-04-23 12:35:56 +0000

    Adding booking list view to bookings.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,13 @@
 let sidenav = document.querySelectorAll('.sidenav');
 M.Sidenav.init(sidenav);
+
+let modal = document.querySelectorAll('.modal');
+M.Modal.init(modal);
+
+var messages = document.getElementsByClassName("messages");
+
+setTimeout(function(){
+   if (messages && messages.length) {
+       messages[0].classList.add('hide');
+   }
+}, 3000);
