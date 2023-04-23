b99b4c8d5cbc9149f2f42c7bbb26976b04a7a4d3
Neillcllghn <neillcllghn@outlook.com>
2023-04-23 11:11:18 +0000

    creating superuser and admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'email', 'day', 'time', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['user', 'email']
+    actions = ['approve_bookings']
+
+    def approve_bookings(self, request, queryset):
+        queryset.update(approved=True)
