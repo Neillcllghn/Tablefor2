43dc7f5894e06d16f92f0f1c53376e2f1641ea58
Neillcllghn <neillcllghn@outlook.com>
2023-04-23 13:06:32 +0000

    Adding the collapsible js for menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,9 @@
 let sidenav = document.querySelectorAll('.sidenav');
 M.Sidenav.init(sidenav);
 
+let collapsible = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapsible);
+
 let modal = document.querySelectorAll('.modal');
 M.Modal.init(modal);
 
